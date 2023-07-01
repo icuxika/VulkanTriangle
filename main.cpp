82dba1a2f13f8051aefee01b4f973547139c0992
icuxika <icuxika@outlook.com>
2023-07-01 12:44:57 +0800

    #ifdef __APPLE__ VK_KHR_portability_subset

--- main.cpp
+++ main.cpp
@@ -23,9 +23,12 @@ const std::vector<const char *> validationLayers = {
 };
 
 const std::vector<const char *> deviceExtensions = {
-        VK_KHR_SWAPCHAIN_EXTENSION_NAME,
-        "VK_KHR_portability_subset"
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
 };
+
+#ifdef __APPLE__
+deviceExtensions.push_back("VK_KHR_portability_subset");
+#endif
 
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
